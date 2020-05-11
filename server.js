aba7b896042ae7c14e6652c643408d8f5feb7e81
x-hog <alexey.virin@yandex.ru>
2020-05-11 19:22:11 +0300

    Добавил змейки и вывод файла JS

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const http = require("http")
 const fs = require("fs")
 const server = http.createServer((req, res) => {
     const publicPath = "./public"
-    const body = req.url === `/css/style.css`
-    ? fs.readFileSync(`${publicPath}/css/style.css`, 'utf8')
-    : fs.readFileSync(`${publicPath}/index.html`, 'utf8')
+    let body = null
+    if (req.url === `/css/style.css`) {
+     body = fs.readFileSync(`${publicPath}/css/style.css`, 'utf8')
+    } else if (req.url === `/js/main.js` ) {
+      body = fs.readFileSync(`${publicPath}/js/main.js`, 'utf8')
+    } else {
+        body = fs.readFileSync(`${publicPath}/index.html`, 'utf8')
+    }
+    console.log(body)
     res.end(body)
+    
 })
 
 const port = process.env.PORT || 3000
